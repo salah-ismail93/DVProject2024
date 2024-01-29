a37800138283902816ae10494868ca208cc71df3
Salah Ismail <S5239380@studenti.unige.it>
2024-01-29 14:29:16 +0100

    overlay

--- src/views/WorldContribution.vue
+++ src/views/WorldContribution.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- Heat Map Contribution Chart -->
-  <div class="bg-white px-6 lg:px-8 py-22 main" id="WorldContribution">
-    <div class="mx-auto max-w-5xl text-center">
-      <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">------------------ World Contribution ------------------</h1>
+  <div class="bg-white px-6 lg:px-8 py-22 main WorldContribution" id="WorldContribution">
+    <div class="max-w-5xl text-center">
+      <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">World Contribution</h1>
     </div>
     <div class="mx-auto max-w-5xl">
       <div class="bg-white px-6 lg:px-8 text-center">
@@ -37,5 +37,41 @@ export default {
 h1{
   font-family: "Roboto";
   color:green
+}
+.WorldContribution {
+  background-image: url('/contribution_image.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 600px;
+  position: relative;
+}
+
+.WorldContribution::before,
+.WorldContribution::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 5%;
+}
+
+.WorldContribution::before {
+  top: 0; /* Change from bottom to top */
+  background: linear-gradient(to top, transparent, #ffffff);
+}
+
+.WorldContribution::after {
+  bottom: 0; /* Keep at the bottom */
+  background: linear-gradient(to bottom, transparent, #ffffff);
+}
+
+.WorldContribution h1 {
+  margin-top: 25%;
+  color: #fff; /* Text color */
+  font-size: 4em !important; /* Adjust the font size */
+  font-weight: bold;
+  text-align: left;
+  text-shadow: 20px 20px 40px rgba(0, 0, 0, 1); /* Add a subtle text shadow for better visibility */
 }
 </style>
