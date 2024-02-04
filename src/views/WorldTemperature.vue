56b6d223f301d56e681bacd6a4cc4d3ae9b909fa
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2024-02-04 18:52:11 +0100

    Update WorldTemperature.vue

--- src/views/WorldTemperature.vue
+++ src/views/WorldTemperature.vue
@@ -2,18 +2,19 @@
     <!-- World Temperature Map Chart -->
     <div class="bg-white px-6 lg:px-8 pt-16 main" id="WorldTemperature">
       <div class="mx-auto max-w-5xl text-center">
-        <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">World Temperature</h2>
+        <h1 class="text-4xl font-bold tracking-tight sm:text-4xl">World Temperature</h1>
       </div>
       <div class="mx-auto max-w-5xl">
         <div class="bg-white px-6 lg:px-8 text-center">
           <div class="mx-auto max-w-5xl">
-            <p class="mx-auto mt-6 max-w-2xl text-base leading-8 text-gray-600 text-center">
+            <p class="mx-auto mt-6 max-w-4xl text-base leading-8 text-gray-600 text-center">
              Let's start with a global look at the world temperature over the last 120 years,
-             in the below dynamic map, you can notice the changing in the average temperature over the years in each country, you can pause the slider to 
+             in the below dynamic map, you can notice the changing in the average temperature over the years in each country.
+             <br><b>You can pause the slider to 
              select the country and year you want to investigate more.
-            </p>
-            <p class="mt-4 text-base leading-7 text-black-600 sm:text-2xl">1900-2020</p>
-          </div>
+            </b></p>
+            <p class="mt-4 text-base leading-7 text-black-600 sm:text-2xl">Enjoy our interactive map!</p>
+           </div>
         </div>
       </div>
     </div>
