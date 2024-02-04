26861564696903a620614e6a7e6ff89e8eb4d8eb
Salah Ismail <S5239380@studenti.unige.it>
2024-02-04 20:33:02 +0100

    title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 <template>
   <main>
     <div class="flex screen	justify-center items-center my-6">
+      <h1>"Climate Insights Hub: Explore, Understand, Act"</h1>
       <!-- <info /> -->
     </div>
   </main>
@@ -16,10 +17,17 @@ export default {
   },
 };
 </script>
-<style>
-.screen {
+<style scoped>
+::v-deep .screen {
   height: 70.7vh;
 }
 
+::v-deep h1 {
+  font-size: 4.5rem !important;
+  color: #FFFFFF;
+  font-family: fantasy, sans-serif;
+  font-weight: 1000;
+  text-shadow: 20px 20px 40px rgba(0, 0, 0, 1);
+}
 
 </style>
